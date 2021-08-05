f81facff7530a15727947201498537c43011313a
Vlad Ovchynnykov <ovd4mail@gmail.com>
2021-08-05 12:07:10 +0300

    entry cmp wip

--- harnic/constants.py
+++ harnic/constants.py
@@ -1,2 +1,2 @@
-HEADER_NOT_STRICT_KEYS = ('Cookie', 'Set-Cookie', 'Last-Modified', 'Expires', 'Date')
+HEADER_NOT_STRICT_KEYS = ('Cookie', 'Set-Cookie', 'Last-Modified', 'Expires', 'Date', 'Referer')
 CONTENT_NOT_STRICT_KEYS = ('text',)
