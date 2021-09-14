5a68a9acb95daaebffc7d33a41a71271e1af79a3
Andrey Pokhilko <apc4@ya.ru>
2021-09-14 22:49:43 +0300

    Make it reproducible

--- demo/jenkins.sh
+++ demo/jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-docker pull gcr.io/mimetic-card-241611/harnic/develop
+# docker pull gcr.io/mimetic-card-241611/harnic/develop
 docker build .. -f ../Dockerfile.demo -t jenkins
 docker run -it -p 8080:8080 -v `pwd`/jenkins-data:/var/jenkins_home jenkins
 # -v /var/run/docker.sock:/var/run/docker.sock
